33df4f913715a4714af92189c8e1230bb53e150f
ZumiLabo <a@zumilabo.com>
2026-03-15 21:25:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from dify_plugin import DifyPluginEnv, Plugin
 
-plugin = Plugin(DifyPluginEnv(MAX_REQUEST_TIMEOUT=120))
+plugin = Plugin(
+    DifyPluginEnv(
+        MAX_REQUEST_TIMEOUT=120,
+    )
+)
 
 if __name__ == "__main__":
     plugin.run()
